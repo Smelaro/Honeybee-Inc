615e059928d12d046e0000d144be98ad33680c7f
Smelaro <42629034+Smelaro@users.noreply.github.com>
2018-11-02 18:31:08 -0300

    Add files via upload

--- funciones.py
+++ funciones.py
@@ -7,6 +7,7 @@ def chequeoerror():
             tablacsv = list(csv.DictReader(archivo))
             tablaorden = ["CODIGO","PRODUCTO","CLIENTE","PRECIO","CANTIDAD"]
             lista = []
+
             for i in tablacsv:
                 for a in tablaorden:
                     if i[a] == "" or str(i[a]).isspace():
@@ -42,14 +43,14 @@ def ultventfun():
         for line in tablacsv:
             tabla.append(line)
 
-        for fila in range(len(tabla)):
-            tablafinal.append([])
-            for columna in range(len(tablaorden)):
-                tablafinal[fila].append(tabla[fila][tablaorden[columna]])
+        for fila in range(len(tabla)):  #SE REVISA LA CANTIDAD DE VALORES QUE TIENE LA LISTA TABLA
+            tablafinal.append([])   #SE AGREGA UNA LISTA VACIA A LA LISTA TABLAFINAL
+            for columna in range(len(tablaorden)):  #SE REVISA LA CANTIDAD DE ELEMENTOS EN TABLAORDEN
+                tablafinal[fila].append(tabla[fila][tablaorden[columna]])   #SE GUARDA EL VALOR DE LA TABLA EN EL ORDEN DE TABLAORDEN
 
         x = tablaorden.index("PRECIO")
         for i in range(len(tablafinal)):
-            tablafinal[i][x] = "$"+str(tablafinal[i][x])
+            tablafinal[i][x] = "$"+"%.2f" % float(tablafinal[i][x])
 
         return (tablafinal,tablaorden)
 
@@ -87,38 +88,38 @@ def mostrar(campo, filtro):
 
         x = tablaorden.index("PRECIO")
         for i in range(len(tablafinal)):
-            tablafinal[i][x] = "$"+str(tablafinal[i][x])
+            tablafinal[i][x] = "$"+ "%.2f" % float(tablafinal[i][x])
 
         if campo == "PRODUCTO":
             for cantidad in range(len(tabla)):
                 cant = cant + int(tabla[cantidad]["CANTIDAD"])
         elif campo == "CLIENTE":
             for precio in range(len(tabla)):
-                cant = cant + (int(tabla[precio]["PRECIO"])*int(tabla[precio]["CANTIDAD"]))
+                cant = cant + (float(tabla[precio]["PRECIO"])*int(tabla[precio]["CANTIDAD"]))
 
+        cant = "%.2f" % cant
         return tablafinal, tablaorden, cant, nombre
 
 def mejores(campo):
     with open('ventas.csv','r') as archivo:
         tablacsv = list(csv.DictReader(archivo))
 
-        cant = 10
         tabla = []
         diccionario = {}
 
-        for nombre in tablacsv:
+        for nombre in tablacsv:     #ESTE FOR RECORRE TODA LA TABLA Y GUARDA LOS NOMBRES
             tabla.append(nombre[campo])
-        tabla = list(set(tabla))
+        tabla = list(set(tabla))       #DESECHA LOS DUPLICADOS, QUEDANDO SOLO 1 NOMBRE DE CADA UNO QUE APARECE
 
-        for compra in range(len(tabla)):
+        for compra in range(len(tabla)):    #SE REVISA LA TABLA DE LOS NOMBRES SOLAMENTE
             valor = 0
-            for linea in range(len(tablacsv)):
-                if tablacsv[linea][campo] == tabla[compra]:
+            for linea in range(len(tablacsv)):  #SE REVISA LA TABLA GENERAL, EL ARCHIVO
+                if tablacsv[linea][campo] == tabla[compra]: #SI EL VALOR DE LA TABLA GENERAL ES IGUAL AL NOMBRE DE LA LISTA
                     if campo == "CLIENTE":
-                        valor = valor + (float(tablacsv[linea]["PRECIO"]) * int(tablacsv[linea]["CANTIDAD"]))
+                        valor = valor + (float(tablacsv[linea]["PRECIO"]) * int(tablacsv[linea]["CANTIDAD"]))   #GUARDA EL VALOR DE ESE REGISTRO DE LA TABLA, HACIENDO PRECIO*CANTIDAD
                     elif campo == "PRODUCTO":
-                        valor = valor + int(tablacsv[linea]["CANTIDAD"])
-            diccionario[tabla[compra]] = ("$"+str(valor) if campo == "CLIENTE" else valor)
+                        valor = valor + int(tablacsv[linea]["CANTIDAD"])    #SI LO QUE QUERES ES SOLO GUARDA LA CANTIDAD DE LOS PRODUCTOS GUARDA LA CANTIDAD
+            diccionario[tabla[compra]] = ("$"+ "%.2f" % valor if campo == "CLIENTE" else valor) #SE INGRESA EL NOMBRE DE LA LISTA Y EL RESULTADO DE LA CUENTA
         
         ordenado = sorted(diccionario.items(), key = lambda t:t[1], reverse=True)
         return ordenado
